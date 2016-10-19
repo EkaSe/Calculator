c78cc9039312e6c7e80f8295594cb96989a0cf80
EkaSe <ekasemyash@ya.ru>
2016-10-19 22:16:12 +0300

    Calculator: CharToString

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -12,9 +12,19 @@ namespace Calculator
 			multiply,
 			divide,
 			degree,
-			factorial,
+			factorial
 		};
-		
+
+		static public int CharToDigit (char symbol) {
+			int code = (int) symbol;
+			int digit;
+			if ((code > 47) && (code < 58))
+				digit = code - 48;
+			else
+				digit = -1;
+			return digit;
+		}
+
 		static protected int Priority (double currentOperator){
 			int result = 0;
 			switch ((int) currentOperator) {
@@ -77,80 +87,71 @@ namespace Calculator
 			string result;
 			for (int i = 0; i < input.Length; i++) {
 				string currentSymbol = Convert.ToString (input [i]);
-				switch (currentSymbol) {
-				case "0":
-				case "1":
-				case "2":
-				case "3":
-				case "4":
-				case "5":
-				case "6":
-				case "7":
-				case "8":
-				case "9":
-					currentDigit = Convert.ToDouble(currentSymbol);
+				if (CharToDigit (input [i]) >= 0) {
+					currentDigit = (double) CharToDigit (input [i]);
 					if (mantissaLength == 1)
 						currentNumber = currentNumber * 10 + currentDigit;
 					else {
 						currentNumber = currentNumber + currentDigit * mantissaLength;
 						mantissaLength *= 0.1;
 					}
-					break;
-				case ".": 
-					mantissaLength = 0.1;
-					break;
-				case " ": 
-					break;
-				case "+": 
-					expression.Add (currentNumber);
-					currentNumber = 0;
-					currentDigit = 0;
-					mantissaLength = 1;
-					expression.Add ((double)OperatorCode.plus);
-					break;
-				case "-": 
-					expression.Add (currentNumber);
-					currentNumber = 0;
-					currentDigit = 0;
-					mantissaLength = 1;
-					expression.Add ((double)OperatorCode.minus);
-					break;
-				case "*": 
-					expression.Add (currentNumber);
-					currentNumber = 0;
-					currentDigit = 0;
-					mantissaLength = 1;
-					expression.Add ((double)OperatorCode.multiply);
-					break;
-				case "/": 
-					expression.Add (currentNumber);
-					currentNumber = 0;
-					currentDigit = 0;
-					mantissaLength = 1;
-					expression.Add ((double)OperatorCode.divide);
-					break;
-				case "(": 
-					int parenthesisCount = 1;
-					int j = i;
-					do {
-						j++;
-						if (j == input.Length)
-							return "Invalid string: parenthesis";
-						if (String.Equals (Convert.ToString (input [j]), ")"))
-							parenthesisCount--;
-						if (String.Equals (Convert.ToString (input [j]), "("))
-							parenthesisCount++;
-						if (parenthesisCount < 0)
-							return "Invalid string: parenthesis";
-					} while (parenthesisCount != 0);
-					string underParenthesis = input.Substring (i + 1, j - i - 1);
-					result = ProcessExpression (input.Substring (i + 1, j - i - 1));
-					currentNumber = Convert.ToDouble (result);
-					i = j;
-					break;	
-				default:
-					result = "Invalid symbol: " + currentSymbol;
-					return result;
+				} else {
+					switch (currentSymbol) {
+					case ".": 
+						mantissaLength = 0.1;
+						break;
+					case " ": 
+						break;
+					case "+": 
+						expression.Add (currentNumber);
+						currentNumber = 0;
+						currentDigit = 0;
+						mantissaLength = 1;
+						expression.Add ((double)OperatorCode.plus);
+						break;
+					case "-": 
+						expression.Add (currentNumber);
+						currentNumber = 0;
+						currentDigit = 0;
+						mantissaLength = 1;
+						expression.Add ((double)OperatorCode.minus);
+						break;
+					case "*": 
+						expression.Add (currentNumber);
+						currentNumber = 0;
+						currentDigit = 0;
+						mantissaLength = 1;
+						expression.Add ((double)OperatorCode.multiply);
+						break;
+					case "/": 
+						expression.Add (currentNumber);
+						currentNumber = 0;
+						currentDigit = 0;
+						mantissaLength = 1;
+						expression.Add ((double)OperatorCode.divide);
+						break;
+					case "(": 
+						int parenthesisCount = 1;
+						int j = i;
+						do {
+							j++;
+							if (j == input.Length)
+								return "Invalid string: parenthesis";
+							if (String.Equals (Convert.ToString (input [j]), ")"))
+								parenthesisCount--;
+							if (String.Equals (Convert.ToString (input [j]), "("))
+								parenthesisCount++;
+							if (parenthesisCount < 0)
+								return "Invalid string: parenthesis";
+						} while (parenthesisCount != 0);
+						result = ProcessExpression (input.Substring (i + 1, j - i - 1));
+						currentNumber = Convert.ToDouble (result);
+						i = j;
+						break;	
+					default:
+						result = "Invalid symbol: " + currentSymbol;
+						return result;
+					}
 				}
 			}
 			expression.Add (currentNumber);
