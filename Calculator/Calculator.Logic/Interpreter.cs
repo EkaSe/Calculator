74d90913443901b7aa4a29dde20ad2adea54e867
EkaSe <ekasemyash@ya.ru>
2017-02-28 08:56:47 +0300

    test statements [draft]

--- Calculator/Calculator.Logic/Interpreter.cs
+++ Calculator/Calculator.Logic/Interpreter.cs
@@ -8,31 +8,21 @@ namespace Calculator.Logic
 	{
 		static public VarSet Globals = new VarSet ();
 
-		/*static public string ProcessStatement (string input) {
-			string statement = Parser.SkipSpaces (input);
-			string result;
-			Variables.CreateLocals (); 
-			Expression tree = new ExpressionBuilder (statement).ToExpression ();
-			result = Parser.DoubleToString (tree.Calculate ());
-			Variables.MergeLocals ();
-			return result;
-		}*/
-
 		static public void Run (Func<string> getInput, Func<string, bool> outputAction) {
 			Run (getInput, outputAction, true);
 		}
 
 		static public void Run (Func<string> getInput, Func<string, bool> outputAction, bool clearRun) {
-			if (clearRun)
-				//Variables.ClearDictionaries ();
+			if (clearRun) {
+				//clear UF list
 				Globals = new VarSet ();
+			}
 			bool finish = false;
 			while (!finish) {
 				string input = getInput ();
 				string output;
 				if (Parser.ToLowerCase (input) != "q") {
 					try {
-						//output = ProcessStatement (input);
 						Statement result = StatementSearcher.Run (input);
 						output = result.Process ();
 					} catch (Exception e) {
--- Calculator/Calculator.Tests/TestCalculator.cs
+++ Calculator/Calculator.Tests/TestCalculator.cs
@@ -101,13 +101,59 @@ namespace Calculator
 			CalculatorTest ("1 * 3 * 4", "12");
 			CalculatorTest ("10/7*14", "20");
 			CalculatorTest ("5!", "120");
+			/*?*/CalculatorTest ("0.5!", "");
 			CalculatorTest ("-7+3", "-4");
 			CalculatorTest ("5.5+2.15", "7.65");
 			//CalculatorTest ("4^(1/2)", "2");
 			//CalculatorTest ("0.125^(-1/3)", "2");
 
 			Console.WriteLine ();
-			string[] expressionSet = new string[] {"x=3","x-1"};
+			string[] expressionSet;
+
+			expressionSet = new string[] {""};
+			InterpreterTest ("Statement 0.0", expressionSet, "");
+			expressionSet = new string[] {"1+2"};
+			InterpreterTest ("Statement 0.1", expressionSet, "3");
+			expressionSet = new string[] {"1+2;"};
+			InterpreterTest ("Statement 0.2", expressionSet, "");
+			/*?*/expressionSet = new string[] {"-12; 0.1*10"};
+			InterpreterTest ("Statement 0.3", expressionSet, "1");
+			/*?*/expressionSet = new string[] {"+12.34 \n  0-3!"};
+			InterpreterTest ("Statement 0.4", expressionSet, "-6");
+			/*?*/expressionSet = new string[] {"1+(1+2*(3-2)); \n (5.5+2.15) / 0.5 "};
+			InterpreterTest ("Statement 0.5", expressionSet, "15.3");
+			/*?*/expressionSet = new string[] {"15-0.2 \n "};
+			InterpreterTest ("Statement 0.6", expressionSet, "");
+			/*?*/expressionSet = new string[] {"15-0.2; \n 000"};
+			InterpreterTest ("Statement 0.7", expressionSet, "0");
+
+			expressionSet = new string[] {"var=1+2"};
+			InterpreterTest ("Statement 1.0: Assignment", expressionSet, "var = 3");
+			expressionSet = new string[] {"x=3","x-1"};
+			InterpreterTest ("Statement 1.1: Assignment", expressionSet, "2");
+			expressionSet = new string[] {"x=3; x=x-1"};
+			InterpreterTest ("Statement 1.2: Assignment", expressionSet, "x = 2");
+
+			expressionSet = new string[] {"{x = 1; y = 2; z = x + y }"};
+			InterpreterTest ("Statement 2.0: Block", expressionSet, "x = 1, y = 2, z = 3");
+			/*?*/expressionSet = new string[] {"{x = 1 \n y = 2; z = x + y; }"};
+			InterpreterTest ("Statement 2.1: Block", expressionSet, "x = 1, y = 2, z = 3,");
+
+			expressionSet = new string[] {"UF () => x=1+2"};
+			InterpreterTest ("Statement 3.0: Lambda", expressionSet, "User function UF is defined");
+			expressionSet = new string[] {"UF () => {x=1+2; x = x-1}"};
+			InterpreterTest ("Statement 3.1: Lambda", expressionSet, "User function UF is defined");
+
+			expressionSet = new string[] {""};
+			InterpreterTest ("Statement 4.0: Incorrect input", expressionSet, "");
+
+			expressionSet = new string[] {"{{x=3}; {y = mIn (x, 10) \n}}"};
+			InterpreterTest ("Statement 5.0: Nested", expressionSet, "x = 3");
+
+
+
+
+			expressionSet = new string[] {"x=3","x-1"};
 			InterpreterTest ("Interpreter 1:", expressionSet, "2");
 			expressionSet = new string[] {"x = x - 3"};
 			InterpreterTest ("Interpreter 2: x = x -3 ", expressionSet, "Invalid expression: Cannot assign value to x");
@@ -124,19 +170,19 @@ namespace Calculator
 			InterpreterTest ("Interpreter 7:", expressionSet, "3");
 
 			expressionSet = new string[] {"UF {x=3 \n y = mIn (x, 10)}", "UF", "y"};
-			InterpreterTest ("Interpreter 8: UF", expressionSet, "3");
+			InterpreterTest ("Interpreter 8: Lambda", expressionSet, "3");
 
-			Console.WriteLine ();
+			/*Console.WriteLine ();
 			Expression testTree = new ExpressionBuilder ("2+3").ToExpression ();
-			//Console.Write (testTree.Draw ());
+			Console.Write (testTree.Draw ());
 			testTree = new ExpressionBuilder ("1+(2+3*(4-5))").ToExpression ();
-			//Console.Write (testTree.Draw ());
+			Console.Write (testTree.Draw ());
 			testTree = new ExpressionBuilder ("min (6!, 2)").ToExpression ();
-			//Console.Write (testTree.Draw ());
+			Console.Write (testTree.Draw ());
 			testTree = new ExpressionBuilder ("max (1+(2+3*(4-5)), 6!, sqrt (-8))").ToExpression ();
-			//Console.Write (testTree.Draw ());
+			Console.Write (testTree.Draw ());*/
 
-			MyCollectionTest.MyListTest ();
+			//MyCollectionTest.MyListTest ();
 
 			Console.WriteLine ("\r\n" + FailedTestsCount + " tests failed");
 		}
