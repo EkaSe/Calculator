7fc7b97991f16d973b73736f91050c798a38f6ba
EkaSe <ekasemyash@ya.ru>
2017-05-09 14:50:54 +0300

    issue #8: Functionality Analyzer: Report to file [no class hierarchy]

---
+++ Calculator/FunctionalityAnalyzer/FuncAnalyzerReport.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Calculator.Logic;
+using Calculator.Tests;
+using System.Text;
+using System.IO;
+
+namespace FunctionalityAnalyzer
+{
+	public static class FuncAnalyzerReport
+	{
+		static public void Run (Type childClassType) {
+			Assembly assembly = childClassType.Assembly;
+			string report = GetReportLinqStringBuilder (assembly);
+			PrintReport (report);
+		}
+
+		public static string LogPath {
+			get {
+				if (logPath == null) {
+					string currentPath = Directory.GetCurrentDirectory ();
+					logPath = currentPath.Substring (0, currentPath.IndexOf (@"FunctionalityAnalyzer")) 
+						+ @"FunctionalityAnalyzer/FuncAnLog.txt";
+				}
+				return logPath;
+			}
+		}
+		static string logPath;
+		static bool clearLog = true;
+
+		static bool PrintReport (string report) {
+			try {
+				if (clearLog && File.Exists (LogPath))
+					File.Delete (LogPath);
+				if (!File.Exists (LogPath)) {
+					using (StreamWriter sw = File.CreateText (LogPath)) {
+						sw.WriteLine (report);
+					}
+				} else {
+					using (StreamWriter sw = File.AppendText (LogPath)) {
+						sw.WriteLine (report);
+					}
+				}
+				return true;
+			} catch (Exception e) {
+				Console.WriteLine (e);
+				Console.WriteLine (LogPath);
+				return false;
+			}
+		}
+
+		static string GetReportForeach (Assembly assembly) {
+			StringBuilder report = new StringBuilder ();
+			Console.WriteLine (assembly.GetTypes ());
+			foreach (Type t in assembly.GetTypes()) {
+				report.AppendLine (t.Name);
+				report.AppendLine ("  Fields:");
+				foreach (FieldInfo field in t.GetFields()) {
+					report.AppendLine ("\t" + field);
+				}
+				report.AppendLine ("  Methods:");
+				foreach (MethodInfo method in t.GetMethods()) {
+					report.AppendLine ("\t" + method);
+				}
+			}
+			return report.ToString ();
+		}
+
+		static string GetReportLinqString (Assembly assembly) {
+			return assembly.GetTypes ()
+				.Select ((Type t) => t.Name + t.GetFields ().Aggregate ("\n  Fields:", 
+					(string startValue, FieldInfo field) => startValue + "\n\t" + field.ToString ())
+					+ t.GetMethods ().Aggregate ("\n  Methods:", 
+						(string startValue, MethodInfo method) => startValue + "\n\t" + method.ToString ()) + "\n")
+				.Aggregate ((report, nextClass) => report + nextClass);
+		}
+
+		static string GetReportLinqStringBuilder (Assembly assembly) {
+			return assembly.GetTypes ().Aggregate (new StringBuilder (), (report, nextClass) => {
+					nextClass.GetFields ().Aggregate (report.AppendLine (nextClass.Name).AppendLine ("  Fields:"),
+						(fieldReport, field) => fieldReport.AppendLine ("\t" + field.ToString ()));
+					nextClass.GetMethods ().Aggregate (report.AppendLine ("  Methods:"), 
+						(methodReport, method) => methodReport.AppendLine ("\t" + method.ToString ()));
+					return report;
+				}).ToString ();
+		}
+	}
+}
+
--- Calculator/FunctionalityAnalyzer/Program.cs
+++ Calculator/FunctionalityAnalyzer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using Calculator.Logic;
+using Calculator.Tests;
 using System.Text;
 
 namespace FunctionalityAnalyzer
@@ -9,21 +11,7 @@ namespace FunctionalityAnalyzer
 	{
 		public static void Main (string[] args)
 		{
-			StringBuilder result = new StringBuilder ();
-			Assembly calculatorLogic = typeof(Interpreter).Assembly;
-			Console.WriteLine (calculatorLogic.GetTypes ());
-			foreach (Type t in calculatorLogic.GetTypes()) {
-				result.AppendLine (t.Name);
-				result.AppendLine ("  Fields:");
-				foreach (FieldInfo field in t.GetFields()) {
-					result.AppendLine ("\t" + field);
-				}
-				result.AppendLine ("  Methods:");
-				foreach (MethodInfo method in t.GetMethods()) {
-					result.AppendLine ("\t" + method);
-				}
-			}
-			Console.Write (result.ToString ());
+			FuncAnalyzerReport.Run (typeof(Interpreter));
 		}
 	}
 }
