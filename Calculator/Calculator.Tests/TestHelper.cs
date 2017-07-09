775dc111ef7a89729a1d918f65671802113b8587
EkaSe <ekasemyash@ya.ru>
2017-07-10 01:01:21 +0300

    issue #8: ToDictionarySafe [exception on attempt to convert Enumerable to Dictionary]

--- Calculator/Calculator.Tests/TestHelper.cs
+++ Calculator/Calculator.Tests/TestHelper.cs
@@ -46,6 +46,25 @@ namespace Calculator.Tests
 			}
 		}
 		static string testLogPath;
+
+		/// <param name="keySelector">Function that gets key for the dictionary from the given source element. Usage: el => el.Name</param>
+		/// <param name="onNewKeyValueSelector">Function that gets value for the dictionary in a case when corresponding key not yet exists in the dictionary. Usage: el => el.Value</param>
+		/// <param name="onKeyExistsValueSelector">Function that gets value for the dictionary in a case when corresponding key is already exists in the dictionary. Usage: (el, oldValue) => oldValue + el.Value</param>
+		public static Dictionary<TKey, TValue> ToDictionarySafe<TItem, TKey, TValue>(
+			this IEnumerable<TItem> source,
+			Func<TItem, TKey> keySelector,
+			Func<TItem, TValue> onNewKeyValueSelector,
+			Func<TItem, TValue, TValue> onKeyExistsValueSelector) {
+			Dictionary<TKey, TValue> result = new Dictionary<TKey, TValue> ();
+			foreach (var item in source) {
+				TKey key = keySelector (item);
+				if (result.ContainsKey (key))
+					result [key] = onKeyExistsValueSelector (item, result [key]);
+				else
+					result.Add (key, onNewKeyValueSelector (item));
+			}
+			return result;
+		}
 	}
 
 	public class TestAttribute : Attribute {}
@@ -56,14 +75,11 @@ namespace Calculator.Tests
 	public class CoversAttribute : Attribute {
 		public MethodInfo Method { get; private set; }
 
-		public CoversAttribute (Type type, string methodName) {
-			Method = type.GetMethod (methodName);
-			if (Method == null)
-				OnErrorReceived (type.ToString () + "." + methodName + " not found");
-		}
-
-		public CoversAttribute (Type type, string methodName, Type[] args) {
-			Method = type.GetMethod (methodName, args);
+		public CoversAttribute (Type type, string methodName, Type[] args = null) {
+			if (args == null)
+				Method = type.GetMethod (methodName);
+			else
+				Method = type.GetMethod (methodName, args);
 			if (Method == null)
 				OnErrorReceived (type.ToString () + "." + methodName + " not found");
 		}
@@ -85,26 +101,30 @@ namespace Calculator.Tests
 		public Dictionary <Type, int> TestedMethodsCount  = new Dictionary<Type, int> ();
 
 		public TestCoverage (Assembly assembly) {
-			var methodList = assembly.GetTypes ()
+			var enumCoveredMethods = assembly.GetTypes ()
 				.SelectMany ((testClass) => testClass.GetMethods ()
 					.SelectMany ((mInfo) => Attribute.GetCustomAttributes (mInfo)
-						.Where ((attribute) => attribute.GetType () == typeof(CoversAttribute))
+						.OfType<CoversAttribute> ()
 						.Where ((attribute) => ((CoversAttribute)attribute).Method != null)
-						.Select ((attribute) => {
-							MethodInfo coveredMethod = ((CoversAttribute)attribute).Method;
-							if (!CoveredMethods.ContainsKey (coveredMethod))
-								CoveredMethods.Add (coveredMethod, new List<MethodInfo> ());
-							CoveredMethods [coveredMethod].Add (mInfo);
-							return coveredMethod;
-			})));			
-			methodList.GetEnumerator ();
+						.ToDictionarySafe <Attribute, MethodInfo, List<MethodInfo>> (
+							(attribute) => ((CoversAttribute)attribute).Method,
+							(attribute) => {
+								List<MethodInfo> mInfoList = new List<MethodInfo> ();
+								mInfoList.Add (mInfo);
+								return mInfoList;
+							},
+							(attribute, mInfoList) => {
+								mInfoList.Add (mInfo);
+								return mInfoList;
+							}
+						)));
+			//exception: Specified cast is not valid
+			CoveredMethods = (Dictionary <MethodInfo, List<MethodInfo>>) enumCoveredMethods;
 
-			TestedMethodsCount = methodList.Select ((method) => method.DeclaringType)
+			TestedMethodsCount = CoveredMethods.Select ((method) => method.Key.DeclaringType)
 				.Distinct ()
 				.ToDictionary ((type) => type, (type) => 
 					CoveredMethods.Count ((method1) => method1.Key.DeclaringType == type));
-			TestedMethodsCount.GetEnumerator();
 		}
 	}
 }
-
